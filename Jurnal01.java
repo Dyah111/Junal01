dd71976fbeedee338e8bbcc049f0bcf41ceeafc9
Dyah Ayuning Permata Sari <stars11fight@gmail.com>
2023-09-27 09:31:00 +0700

    commit pertama

---
+++ Jurnal01.java
@@ -0,0 +1,9 @@
+public class Jurnal01{
+
+    public static void main(String[] args) {
+
+        System.out.println ("NAMA : DYAH AYUNING PERMATA SARI");
+        System.out.println ("NIM : 607062300134");
+        System.out.println ("KELAS : D3IF-47-04");
+    }
+}
